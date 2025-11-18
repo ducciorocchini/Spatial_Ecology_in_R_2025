1134ef3c1f3864b8eb629aa022eef8804366c5a5
Duccio Rocchini <ducciorocchini@gmail.com>
2025-11-18 16:53:07 +0100

    Update 05_community_overlap.R

--- Code/05_community_overlap.R
+++ Code/05_community_overlap.R
@@ -12,6 +12,38 @@ kerinci$circ <- kerinci$Time * 2 * pi
 
 tiger <- kerinci[kerinci$Sps=="tiger",]
 
-timetiger <- tiger$circ
-densityPlot(timetiger)
+tigertime <- tiger$circ
+densityPlot(tigertime)
 
+densityPlot(tiger$circ)
+
+# Exercise: repeat the graph for the macaque
+
+macaque <- kerinci[kerinci$Sps=="macaque",]
+macaquetime <- macaque$circ
+
+densityPlot(macaque$circ)
+
+# seeing the overlap between times of different species
+overlapPlot(tigertime, macaquetime)
+
+
+
+#----
+
+# Get the unique species names
+species_list <- unique(kerinci$Sps)
+
+# Set up the plotting area with a grid (adjust n and m based on the number of species)
+par(mfrow = c(3, 3))  # Example: 3 rows and 3 columns (adjust as needed)
+
+# Loop through each species and create density plots
+for (species in species_list) {
+  # Subset data for the current species
+  species_data <- kerinci[kerinci$Sps == species, ]
+  
+  # Create a density plot for the 'circ' variable of the current species
+  plot(density(species_data$circ), 
+       main = paste("Density Plot of Circumference for", species), 
+       xlab = "Circumference")
+}

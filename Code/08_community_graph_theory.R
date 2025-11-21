beda2add4135b2715ef2f75eeb94918c489682fc
Duccio Rocchini <ducciorocchini@gmail.com>
2025-11-21 09:34:17 +0100

    Update 08_community_graph_theory.R

--- Code/08_community_graph_theory.R
+++ Code/08_community_graph_theory.R
@@ -10,6 +10,20 @@ prey <- c("Algae", "Zooplankton", "Small Fish", "Small Fish", "Large Fish")
 
 interactions <- data.frame(predator, prey)
 
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+plot(g)
+
+g <- graph_from_data_frame(interactions, vertices = species, directed = F)
+plot(g)
+
+# set.seed() function
+
+set.seed(42)
+g <- graph_from_data_frame(interactions, vertices = species, directed = T)
+plot(g)
+
+
+
 
 
 

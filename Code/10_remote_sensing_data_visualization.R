f27d6dccbd66828121800895f9a0f70ed5c71e50
Duccio Rocchini <ducciorocchini@gmail.com>
2025-11-21 09:57:14 +0100

    Create 10_remote_sensing_data_visualization.R

---
+++ Code/10_remote_sensing_data_visualization.R
@@ -0,0 +1,10 @@
+# Code to visualize remote sensing data
+
+# Zenodo set: https://zenodo.org/records/15645465
+# install.packages("imageRy")
+
+library(imageRy)
+library(terra)
+
+# Listing data inside imageRy
+im.list()
